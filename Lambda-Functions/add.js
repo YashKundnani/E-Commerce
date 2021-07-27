e9c2e2d6fedf766f9da78c1bb9240f613c461662
YashKundnani <43103704+YashKundnani@users.noreply.github.com>
2021-07-27 17:27:30 +0530

    Added Lambda Function Code

---
+++ Lambda-Functions/add.js
@@ -0,0 +1,41 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+exports.handler = async(event,context) => {
+ const documentClient = new AWS.DynamoDB.DocumentClient();
+
+ let responseBody = "";
+ let statusCode = 0;
+ 
+ const {id, productname} =JSON.parse(event.body);
+
+ const params = {
+    TableName: "Products",
+    Item: {
+      id: id,
+      productname: productname
+    }
+  };
+
+try{
+   const data = await documentClient.put(params).promise();
+   responseBody = JSON.stringify(data);
+   statusCode = 201;
+ } catch(err) {
+  responseBody = `Unable to put product: ${err}`;
+  statusCode = 403;
+ }
+ 
+ const response = {
+  statusCode: statusCode,
+  headers: {
+   "Content-Type": "application/json",
+   "access-control-allow-origin": "*",
+   "Access-control-allow-methods": "POST, DELETE, PATCH"
+   
+ },
+ body: responseBody
+ };
+ 
+  return response;
+};
